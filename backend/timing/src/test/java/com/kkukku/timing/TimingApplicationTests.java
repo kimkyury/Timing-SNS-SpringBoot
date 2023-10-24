92ab56d989bbbcd597a3dae95101e0dce7f1bd19
unit74 <u__nit@naver.com>
2023-10-24 15:01:24 +0900

    🎉 feat: Init project (Spring Boot 3.2 RC-1, JDK 21)

---
+++ backend/timing/src/test/java/com/kkukku/timing/TimingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkukku.timing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
