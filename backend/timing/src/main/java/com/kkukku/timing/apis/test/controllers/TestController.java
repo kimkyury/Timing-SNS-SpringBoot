ce317da85c197194f14d61a4f0a9c7cda968c1e9
김규리 <mandarining0918@gmail.com>
2023-10-26 15:36:49 +0900

    � feat: Setting SpringDoc for Swagger

--- backend/timing/src/main/java/com/kkukku/timing/apis/test/controllers/TestController.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/test/controllers/TestController.java
@@ -1,11 +1,13 @@
 package com.kkukku.timing.apis.test.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "Test", description = "Test API")
 public class TestController {
 
     @GetMapping("/ping")
