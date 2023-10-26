f1fad431fe11ce6c8bf864150095e09cc3f58f79
김규리 <mandarining0918@gmail.com>
2023-10-26 14:46:01 +0900

    ✅ test: create entity and repository for Test

---
+++ backend/timing/src/main/java/com/kkukku/timing/apis/test/repositories/HashTagOptionRepository.java
@@ -0,0 +1,8 @@
+package com.kkukku.timing.apis.test.repositories;
+
+import com.kkukku.timing.apis.test.entities.HashTagOptionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashTagOptionRepository extends JpaRepository<HashTagOptionEntity, Long> {
+
+}
