cbc5f8d364791a9d93bd37c033128dc29cd4bce8
김규리 <mandarining0918@gmail.com>
2023-10-26 16:30:23 +0900

    � refactor: Move File

--- backend/timing/src/main/java/com/kkukku/timing/apis/hashTagOption/repositories/HashTagOptionRepository.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/hashTagOption/repositories/HashTagOptionRepository.java
@@ -1,6 +1,6 @@
-package com.kkukku.timing.apis.test.repositories;
+package com.kkukku.timing.apis.hashTagOption.repositories;
 
-import com.kkukku.timing.apis.test.entities.HashTagOptionEntity;
+import com.kkukku.timing.apis.hashTagOption.entities.HashTagOptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HashTagOptionRepository extends JpaRepository<HashTagOptionEntity, Long> {
