20877c85eb24f2a6b895a29e6adff6607ac99a86
unit74 <u__nit@naver.com>
2023-10-27 17:51:28 +0900

    📝 chore: Edit database primary key

--- backend/timing/src/main/resources/data.sql
+++ backend/timing/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO members (id, email, profile_image_url, nickname, birthyear, gender)
-VALUES (1, 'test@com', '/test.png', '테스터', 2000, 1),
-       (2, 'test2@com', '/test.png', '테스터2', 1998, 0);
+INSERT INTO members (id, email, profile_image_url, nickname, birth_year, gender)
+VALUES (100, 'test@com', '/test.png', '테스터', 2000, 'M'),
+       (101, 'test2@com', '/test.png', '테스터2', 1998, 'F');
 
 INSERT INTO hash_tag_options (id, content)
 VALUES (1, '운동'),
@@ -9,9 +9,9 @@ VALUES (1, '운동'),
        (4, '저녁');
 
 INSERT INTO challenges (id, member_id, started_at, ended_at, goal_contents)
-VALUES (1, 1, '2023-10-01', '2023-10-22', '테스트목표'),
-       (2, 1, '2023-10-02', '2023-10-23', '테스트목표2'),
-       (3, 1, '2023-10-03', '2023-10-24', NULL);
+VALUES (1, 100, '2023-10-01', '2023-10-22', '테스트목표'),
+       (2, 100, '2023-10-02', '2023-10-23', '테스트목표2'),
+       (3, 101, '2023-10-03', '2023-10-24', NULL);
 
 INSERT INTO challenge_hash_tags (id, hash_tag_options_id, challenge_id)
 VALUES (3, 3, 2),
@@ -25,17 +25,17 @@ VALUES (1, 2, '/test.png', '2023-10-03'),
 
 INSERT INTO feeds (id, member_id, parent_id, root_id, started_at, ended_at,
                    goal_contents, thumbnail_url, timelapse_url, is_private)
-VALUES (1, 1, NULL, NULL, '2023-10-02', '2023-10-23', '테스트목표', '/test.png', '/test.mp4', 1);
+VALUES (1, 100, NULL, NULL, '2023-10-02', '2023-10-23', '테스트목표', '/test.png', '/test.mp4', 1);
 
 INSERT INTO feed_hash_tags (id, hash_tag_options_id, feed_id)
 VALUES (1, 2, 1),
        (2, 4, 1);
 
 INSERT INTO comments (id, feed_id, member_id, content)
-VALUES (1, 1, 1, '인증이에요');
+VALUES (1, 1, 101, '인증이에요');
 
 
 INSERT INTO likes (id, feed_id, member_id)
-VALUES (2, 1, 2);
+VALUES (2, 1, 101);
 
 
