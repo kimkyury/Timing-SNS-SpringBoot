c865fbc0f94d711560e7676719eb58c295cb0f6c
unit74 <u__nit@naver.com>
2023-10-27 17:47:21 +0900

    ⚡️ feat: Add oauth2 success handler

--- backend/timing/src/main/java/com/kkukku/timing/oauth2/handlers/OAuth2AuthenticationSuccessHandler.java
+++ backend/timing/src/main/java/com/kkukku/timing/oauth2/handlers/OAuth2AuthenticationSuccessHandler.java
@@ -1,37 +1,64 @@
 package com.kkukku.timing.oauth2.handlers;
 
+import com.kkukku.timing.jwt.services.JwtService;
+import com.kkukku.timing.redis.services.RedisService;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+
+    private final RedisService redisService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException, ServletException {
         DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
 
-        String id = defaultOAuth2User.getAttributes()
-                                     .get("id")
-                                     .toString();
-        String body = """
-            {"id":"%s"}
-            """.formatted(id);
+        String email = ((Map<String, Object>) defaultOAuth2User.getAttributes()
+                                                               .get("kakao_account")).get("email")
+                                                                                     .toString();
 
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        String accessToken = jwtService.generateAccessToken(email);
+        String refreshToken = jwtService.generateRefreshToken(email);
+
+        String referer = request.getHeader("Referer");
+        String redirectURI = UriComponentsBuilder.fromUriString(referer)
+                                                 .path("/oauth2/authorization/kakao")
+                                                 .queryParam("access-token", accessToken)
+                                                 .build()
+                                                 .toString();
+
+        Cookie cookie = new Cookie("refresh-token", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(jwtService.getRefreshTokenExpiration()
+                                   .intValue());
+
+        response.addCookie(cookie);
+        response.setContentType("application/json");
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
-        PrintWriter writer = response.getWriter();
-        writer.println(body);
-        writer.flush();
+        redisService.saveDataWithTimeout("member:" + email + ":RT", refreshToken,
+            jwtService.getRefreshTokenExpiration());
+
+        response.sendRedirect(redirectURI);
     }
 }
