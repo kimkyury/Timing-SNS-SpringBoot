c566e49f8ed148f11919ca93f283333e4bdca9c4
Kim JeongHui <spor1998@naver.com>
2023-10-31 18:02:49 +0900

    :art: Refactor: HomePage refactoring

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,9 @@
 import styles from './Header.module.css';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Logo from '../../assets/Logo.png';
+
 function Header() {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const currentUrl = location.pathname;
     return (
         <div className={styles.header}>
-            <div className={styles.title}>
-                <img src={Logo} className={styles.Logo} />
-            </div>
+            <div className={styles.title}>Timing</div>
         </div>
     );
 }
--- frontend/src/components/TimeLapse/TimeLapse.jsx
+++ frontend/src/components/TimeLapse/TimeLapse.jsx
@@ -3,22 +3,48 @@ import { useNavigate } from 'react-router-dom';
 import { CircularProgressbar, buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import dog from '../../assets/dog2.jpg';
+import { useEffect, useState } from 'react';
+import { element } from 'prop-types';
 function TimeLapse() {
-    const state = {
-        categoryList: [
+    const navigate = useNavigate();
+    const [timeLaps, setTimeLaps] = useState([]);
+
+    useEffect(() => {
+        // 여기서 timeLaps 가져올꺼임
+        const state = [
             { percent: 10, name: 'test1', img: `${dog}` },
             { percent: 50, name: 'test2', img: `${dog}` },
-            { percent: 70, name: 'test3', img: `${dog}` },
-        ],
-    };
-    const navigate = useNavigate();
-    function CreateFeed() {
-        navigate(`/create`);
-    }
+            // { percent: 70, name: 'test3', img: `${dog}` },
+            // { percent: 70, name: 'test3', img: `${dog}` },
+            // { percent: 70, name: 'test3', img: `${dog}` },
+        ];
+
+        setTimeLaps(state);
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.timeContainer}>
-                {Array.isArray(state.categoryList) &&
+                {timeLaps.map((element, index) => (
+                    <div key={index} className={styles.TimeLapse}>
+                        <CircularProgressbarWithChildren
+                            value={element.percent}
+                            strokeWidth="10"
+                            styles={buildStyles({ pathColor: 'red', textColor: 'black' })}
+                        >
+                            <img
+                                src={element.img}
+                                style={{
+                                    width: '48px',
+                                    height: '48px',
+                                    borderRadius: '30px',
+                                    objectFit: 'cover',
+                                }}
+                            />
+                        </CircularProgressbarWithChildren>
+                    </div>
+                ))}
+                {/* {Array.isArray(state.categoryList) &&
                     state.categoryList.map((v, i) => (
                         <div key={i} className={styles.TimeLapse}>
                             <CircularProgressbarWithChildren
@@ -37,8 +63,8 @@ function TimeLapse() {
                                 />
                             </CircularProgressbarWithChildren>
                         </div>
-                    ))}
-                <div className={styles.TimeLapse} onClick={CreateFeed}>
+                    ))} */}
+                <div className={styles.TimeLapse} onClick={() => navigate(`/create`)}>
                     <CircularProgressbar
                         value="40"
                         text="+"
