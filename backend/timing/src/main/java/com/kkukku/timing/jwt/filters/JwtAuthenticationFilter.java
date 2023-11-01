75345982de7cbf523cb4d8da246887cc69260876
unit74 <u__nit@naver.com>
2023-11-01 09:09:00 +0900

    🎨 Refactor: Modify filter structure
    - 정확히 JWT가 들어온 경우 Spring Security Context Holder에 Authentication을 저장하는 역할로 변경

--- backend/timing/src/main/java/com/kkukku/timing/jwt/filters/JwtAuthenticationFilter.java
+++ backend/timing/src/main/java/com/kkukku/timing/jwt/filters/JwtAuthenticationFilter.java
@@ -3,14 +3,12 @@ package com.kkukku.timing.jwt.filters;
 
 import com.kkukku.timing.jwt.services.JwtService;
 import com.kkukku.timing.redis.services.RedisService;
-import com.kkukku.timing.response.codes.ErrorCode;
 import com.kkukku.timing.security.services.MemberDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -40,33 +38,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (jwt != null) {
             String email = jwtService.extractUsername(jwt);
 
-            if (email == null) {
-                response.sendError(ErrorCode.INVALID_TOKEN_SUBJECT.getStatus(),
-                    ErrorCode.INVALID_TOKEN_SUBJECT.getMessage());
-                return;
+            if (email != null && jwtService.isTokenValid(jwt, email)) {
+                String isLogout = redisService.getValue("member:" + email + ":LOGOUT");
+
+                if (isLogout == null) {
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        memberDetailService.loadUserByUsername(email), null,
+                        AuthorityUtils.NO_AUTHORITIES);
+
+                    SecurityContextHolder.getContext()
+                                         .setAuthentication(authentication);
+                }
             }
-
-            if (!jwtService.isTokenValid(jwt, email)) {
-                response.sendError(ErrorCode.INVALID_TOKEN.getStatus(),
-                    ErrorCode.INVALID_TOKEN.getMessage());
-                return;
-            }
-
-            String isLogout = redisService.getValue("member:" + email + ":LOGOUT");
-
-            if (Optional.ofNullable(isLogout)
-                        .isEmpty()) {
-                response.sendError(ErrorCode.LOGGED_OUT_MEMBER_TOKEN.getStatus(),
-                    ErrorCode.LOGGED_OUT_MEMBER_TOKEN.getMessage());
-                return;
-            }
-
-            Authentication authentication = new UsernamePasswordAuthenticationToken(
-                memberDetailService.loadUserByUsername(email), null,
-                AuthorityUtils.NO_AUTHORITIES);
-
-            SecurityContextHolder.getContext()
-                                 .setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
