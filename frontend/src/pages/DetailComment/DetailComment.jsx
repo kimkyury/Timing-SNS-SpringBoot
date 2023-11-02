34c44651e684918691641e90d2834c0c46da7938
Kim JeongHui <spor1998@naver.com>
2023-11-02 12:42:04 +0900

    :art: Refactor: refactor feed

--- frontend/src/pages/DetailComment/DetailComment.jsx
+++ frontend/src/pages/DetailComment/DetailComment.jsx
@@ -1,71 +1,10 @@
 import styles from './DetailComment.module.css';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import MainFeed from '../../components/Feed/MainFeed';
-import dog from '../../assets/dog.jpg';
+import Feed from '../../components/Feed/Feed';
+
 function DetailComment() {
-    const location = useLocation();
-    const state = location.state;
-    const [comments, setComments] = useState(state.comments);
-    const [newComment, setNewComment] = useState('');
-    const user = { id: 'aabbccdd', profile_img: `${dog}` };
-
-    function handleAddComment() {
-        if (newComment.trim() !== '') {
-            const updatedComments = [...comments];
-            updatedComments.push({
-                profileimage: user.profile_img,
-                name: user.id,
-                comment: newComment,
-            });
-            setComments(updatedComments);
-            setNewComment(''); // 댓글 추가 후 새 댓글 상태 초기화
-        }
-    }
-    function handleDeleteComment(commentIndex) {
-        const commentToDelete = comments[commentIndex];
-        if (commentToDelete.name === user.id) {
-            const updatedComments = [...comments];
-            updatedComments.splice(commentIndex, 1);
-            setComments(updatedComments);
-        }
-    }
-    function activeEnter(e) {
-        if (e.key === 'Enter') {
-            handleAddComment();
-        }
-    }
-
     return (
         <div className={styles.container}>
-            <MainFeed />
-            <div className={styles.createbox}>
-                <img src={user.profile_img} className={styles.imagebox} />
-                <input
-                    type="text"
-                    className={styles.inputbox}
-                    placeholder="댓글추가"
-                    value={newComment}
-                    onChange={(e) => setNewComment(e.target.value)}
-                    onKeyDown={(e) => activeEnter(e)}
-                />
-                <button onClick={handleAddComment} className={styles.btn}>
-                    추가
-                </button>
-            </div>
-            <div className={styles.commentContainer}>
-                {comments.map((v, i) => (
-                    <div key={i} className={styles.commentbox}>
-                        <img src={v.profileimage} className={styles.imagebox} />
-                        <div className={styles.innerbox}>
-                            <div className={styles.commentname}>@{v.name}</div>
-                            <div className={styles.commentcontent}>{v.comment}</div>
-                        </div>
-                        {user.id == v.name ? <CloseOutlinedIcon onClick={() => handleDeleteComment(i)} /> : ''}
-                    </div>
-                ))}
-            </div>
+            <Feed />
         </div>
     );
 }
--- frontend/src/pages/MainFeed/MainFeed.jsx
+++ frontend/src/pages/MainFeed/MainFeed.jsx
@@ -1,115 +1,12 @@
-import { useEffect, useState } from 'react';
 import styles from './MainFeed.module.css';
-import { useNavigate, useLocation } from 'react-router-dom';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
-import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import dog from '../../assets/dog.jpg';
-import { style, width } from '@mui/system';
+import Feed from '../../components/Feed/Feed';
 
 function MainFeed() {
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const [user, setUser] = useState(null);
-    const [state, setState] = useState([]);
-
-    useEffect(() => {
-        const user = { id: '김정희', profile_img: `${dog}` };
-        setUser(user);
-        const state = {
-            pk: 1,
-            profileimage: `${dog}`,
-            name: '하성호',
-            id: '@헬린이',
-            image: `${dog}`,
-            isLiked: false,
-            likes: 1234567,
-            comment: 765432,
-            share: 1000,
-            hash: ['#개', '#댕댕이', '#시바', '#산책'],
-            content: '멍멍',
-            comments: [
-                { name: '시바', comment: '뭐', profileimage: `${dog}` },
-                { name: '씨바', comment: 'ㅋㅋㅋ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-                { name: '치바', comment: 'ㅎㅎㅎㅎ', profileimage: `${dog}` },
-            ],
-        };
-        setState(state);
-    }, []);
-
-    function gotoDetailFeed() {
-        navigate(`/detailfeed/${state.pk}`, { state });
-    }
-
-    function formatK(count) {
-        if (count >= 100000) {
-            return (count / 1000000).toFixed(1) + 'm';
-        } else if (count >= 1000) {
-            return (count / 1000).toFixed(1) + 'k';
-        } else {
-            return count;
-        }
-    }
-
     return (
         <div className={styles.container}>
-            <div className={styles.nameContainer}>
-                <img src={state.profileimage} className={styles.profileimage} />
-                <div className={styles.namebox}>
-                    <div className={styles.name}>{state.name}</div>
-                    <div className={styles.id}>{state.id}</div>
-                </div>
-            </div>
-
-            <img src={state.image} className={styles.imageContainer} onClick={gotoDetailFeed} />
-
-            <div className={styles.tagContainer}>
-                <div className={styles.tagitem}>
-                    <div>
-                        {state.isLiked ? (
-                            <FavoriteOutlinedIcon style={{ width: '4vw', height: '4vw' }} />
-                        ) : (
-                            <FavoriteBorderOutlinedIcon style={{ width: '4vw', height: '4vw' }} />
-                        )}
-                    </div>
-                    <div>{formatK(state.likes)}</div>
-                </div>
-                <div className={styles.tagitem}>
-                    <div>
-                        <SmsOutlinedIcon style={{ width: '4vw', height: '4vw' }} />
-                    </div>
-                    <div>{formatK(state.comment)}</div>
-                </div>
-                <div className={styles.tagitem}>
-                    <div>
-                        <ShareOutlinedIcon style={{ width: '4vw', height: '4vw' }} />
-                    </div>
-                    <div>{formatK(state.share)}</div>
-                </div>
-            </div>
-
-            <div className={styles.commentContainer}>
-                <div className={styles.commentname}>{state.name}</div>
-                <div className={styles.commentcontent}>{state.content}</div>
-            </div>
-            <div className={styles.hashTagContainer}>
-                {state.length != 0 &&
-                    state.hash.map((v, i) => (
-                        <div key={i} className={styles.hash}>
-                            <div>{v}</div>
-                        </div>
-                    ))}
-            </div>
+            <Feed />
+            <Feed />
+            <Feed />
         </div>
     );
 }
