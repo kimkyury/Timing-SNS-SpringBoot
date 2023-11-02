0dce33d31cbc38ddca96325537a068655f0f5ed2
김규리 <mandarining0918@gmail.com>
2023-11-02 09:56:49 +0900

    � fix: Fix MainTestCode

--- backend/timing/src/test/java/com/kkukku/timing/TimingApplicationTests.java
+++ backend/timing/src/test/java/com/kkukku/timing/TimingApplicationTests.java
@@ -1,13 +1,13 @@
 package com.kkukku.timing;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
 class TimingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertEquals(42, Integer.sum(19, 23));
+    }
 }
