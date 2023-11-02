e7db862240c0b0227727801dd21e4427ec28aa8f
unknown <tgx963@naver.com>
2023-11-02 14:05:43 +0900

    :lipstick: Style  : 피드생성  css수정

--- frontend/src/pages/CreateFeed/CreateFeed.jsx
+++ frontend/src/pages/CreateFeed/CreateFeed.jsx
@@ -1,18 +1,73 @@
-import styles from "./CreateFeed.module.css"
-import Input from '@mui/joy/Input';
-function CreateFeed() {
+import { useState } from "react";
+import styles from "./CreateFeed.module.css";
+import Input from "@mui/joy/Input";
+import Textarea from "@mui/joy/Textarea";
+import Calendar from "react-calendar";
+import moment from "moment";
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.boxname}>시작일</div>
-            <Input type="date" />
-            <div className={styles.boxname}>#태그</div>
-            <Input type="text" />
-            <div className={styles.boxname}>달성목표</div>
-            <Input type="text" />
-            <button className={styles.btn}>제출</button>
-        </div>
-    )
-  }
-  
-  export default CreateFeed
+import "./Calendar.css";
+function CreateFeed() {
+  const [tags, setTags] = useState([]);
+  const [currentTag, setCurrentTag] = useState("");
+  const [value, onChange] = useState(new Date());
+  const handleTagInputChange = (event) => {
+    setCurrentTag(event.target.value);
+  };
+
+  const handleTagAdd = () => {
+    if (currentTag.trim() !== "") {
+      setTags([...tags, "# " + currentTag]);
+      setCurrentTag("");
+    }
+  };
+
+  const handleTagRemove = (index) => {
+    const updatedTags = [...tags];
+    updatedTags.splice(index, 1);
+    setTags(updatedTags);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.boxname}>시작일</div>
+      <Calendar onChange={onChange} value={value} className={styles.calender} />
+      <div>{moment(value).format("YYYY년 MM월 DD일")} </div>
+      <div className={styles.boxname}>해쉬</div>
+      <div className={styles.tagInputContainer}>
+        <Input
+          type="text"
+          value={currentTag}
+          onChange={handleTagInputChange}
+          className={styles.inputbox}
+          placeholder="#"
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              handleTagAdd();
+            }
+          }}
+        />
+        <button onClick={handleTagAdd} className={styles.btn}>
+          추가
+        </button>
+      </div>
+      <div className={styles.tagList}>
+        {tags.map((tag, index) => (
+          <div key={index} className={styles.tag}>
+            {tag}
+            <button
+              className={styles.tagbtn}
+              onClick={() => handleTagRemove(index)}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className={styles.boxname}>달성목표</div>
+      <Textarea minRows={4} className={styles.contentbox} />
+      <button className={styles.summitbtn}>챌린지 시작하기</button>
+    </div>
+  );
+}
+
+export default CreateFeed;
--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,18 +1,46 @@
-import TimeLapse from "../../components/TimeLapse/TimeLapse"
-import styles from "./Profile.module.css"
-import UserProfile from "../../components/UserProfile/USerProfile"
-
+import TimeLapse from "../../components/TimeLapse/TimeLapse";
+import styles from "./Profile.module.css";
+import UserProfile from "../../components/UserProfile/USerProfile";
+import MyFeed from "../../components/Feed/MyFeed";
+import dog from "../../assets/dog.jpg";
 function Profile() {
+  const state = {
+    contents: [
+      { image: `${dog}`, isPublic: true },
+      { image: `${dog}`, isPublic: false },
+      { image: `${dog}`, isPublic: false },
+      { image: `${dog}`, isPublic: false },
+      { image: `${dog}`, isPublic: false },
+      { image: `${dog}`, isPublic: true },
+      { image: `${dog}`, isPublic: false },
+      { image: `${dog}`, isPublic: true },
+      { image: `${dog}`, isPublic: false },
+      { image: `${dog}`, isPublic: false },
+    ],
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.user_info}>
+        <UserProfile />
+      </div>
+      <div className={styles.proccessing_timelapse}>
+        <div className={styles.timeContainerName}>진행중인 타입랩스</div>
+        <TimeLapse />
+      </div>
+      <div className={styles.my_timelapse}>
+        <div className={styles.timeContainerName}>공개 타입랩스</div>
+        <MyFeed
+          state={state.contents.filter((content) => content.isPublic == false)}
+        />
+      </div>
+      <div className={styles.my_timelapse}>
+        <div className={styles.timeContainerName}>비공개 타입랩스</div>
+        <MyFeed
+          state={state.contents.filter((content) => content.isPublic == true)}
+        />
+      </div>
+    </div>
+  );
+}
 
-    return (
-        <div className={styles.container}>
-          <div className={styles.user_info}><UserProfile/></div>
-          <div className={styles.proccessing_timelapse}>
-            <TimeLapse/>
-          </div>
-          <div className={styles.my_timelapse}>my_timelapse</div>
-        </div>
-    )
-  }
-  
-  export default Profile
+export default Profile;
