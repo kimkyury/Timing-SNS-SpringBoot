aacd56bffdb053f571fc0a6c870cbac43839c7f0
김규리 <mandarining0918@gmail.com>
2023-11-02 12:27:02 +0900

    ✨ Feat: Implement Member Get(Own/other) Controller

--- backend/timing/src/main/java/com/kkukku/timing/apis/member/controllers/MemberController.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/member/controllers/MemberController.java
@@ -1,6 +1,7 @@
 package com.kkukku.timing.apis.member.controllers;
 
 import com.kkukku.timing.apis.member.requests.MemberRegisterRequest;
+import com.kkukku.timing.apis.member.responses.MemberDetailResponse;
 import com.kkukku.timing.apis.member.services.MemberService;
 import com.kkukku.timing.response.ApiResponseUtil;
 import com.kkukku.timing.security.utils.SecurityUtil;
@@ -10,8 +11,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +28,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @Operation(summary = "Member의 가입 후 최초 접근, 정보 등록", tags = {"1. Member"})
-    @PatchMapping(value = "/employees", consumes = {
+    @PatchMapping(value = "/", consumes = {
         MediaType.MULTIPART_FORM_DATA_VALUE
     })
     public ResponseEntity<Void> registerMember(
@@ -38,5 +41,24 @@ public class MemberController {
         return ApiResponseUtil.success();
     }
 
+    @Operation(summary = "Member의 본인 정보 조회", tags = {"1. Member"})
+    @GetMapping(value = "/")
+    public ResponseEntity<MemberDetailResponse> getOwnMemberInfo() {
+
+        String memberEmail = SecurityUtil.getLoggedInMemberEmail();
+        MemberDetailResponse memberDetailResponse = memberService.getMemberInfo(memberEmail);
+
+        return ApiResponseUtil.success(memberDetailResponse);
+    }
+
+    @Operation(summary = "Member의 타 멤버 정보 조회", tags = {"1. Member"})
+    @GetMapping(value = "/")
+    public ResponseEntity<MemberDetailResponse> getOtherMemberInfo(
+        @RequestParam(name = "email", required = false) String memberEmail) {
+
+        MemberDetailResponse memberDetailResponse = memberService.getMemberInfo(memberEmail);
+
+        return ApiResponseUtil.success(memberDetailResponse);
+    }
 
 }
