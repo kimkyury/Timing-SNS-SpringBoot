e8b0009c64ad078beb9fc94a1ce1ab934e6b57c5
unit74 <u__nit@naver.com>
2023-11-03 17:48:36 +0900

    ✨ Feat: Add feeds table entity, repository, service

---
+++ backend/timing/src/main/java/com/kkukku/timing/apis/feed/repositories/FeedRepository.java
@@ -0,0 +1,8 @@
+package com.kkukku.timing.apis.feed.repositories;
+
+import com.kkukku.timing.apis.feed.entities.FeedEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedRepository extends JpaRepository<Long, FeedEntity> {
+
+}
