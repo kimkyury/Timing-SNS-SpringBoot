157746bc0c83d0f1813e3e02537a03bf9e8cf011
unknown <tgx963@naver.com>
2023-11-03 17:31:52 +0900

    :art:Refactor : comment수정

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -5,8 +5,8 @@ import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlin
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import SmsOutlinedIcon from "@mui/icons-material/SmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import dog from "../../assets/dog.jpg";
-
 function Feed() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -17,117 +17,145 @@ function Feed() {
   const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
-    const user = { id: "김정희", profile_img: `${dog}` };
+    const user = {
+      memberId: 12,
+      email: "doglover18@naver.com",
+      nickname: "김정희",
+      profileImg: `${dog}`,
+    };
     setUser(user);
     const state = {
-      pk: 1,
-      profileimage: `${dog}`,
-      name: "하성호",
-      id: "@헬린이",
-      image: `${dog}`,
-      isLiked: false,
-      isPublic: true,
-      likes: 1234567,
-      comment: 765432,
-      share: 1000,
-      time: new Date() - 400,
-      hash: ["#개", "#댕댕이", "#시바", "#산책"],
-      content: "멍멍",
+      id: 1,
+      user: {
+        memberId: 12,
+        email: "doglover18@naver.com",
+        nickname: "김정희",
+        profileImg: `${dog}`,
+      },
+      parentId: null,
+      rootId: null,
+      goalContent: "몸짱",
+      content: "안녕하세요!!",
+      thumbnailUrl: `${dog}`,
+      isPrivate: true,
+      isLike: true,
+      time: new Date(),
+      hashTag: ["#운동", "#오운완"],
+      likeCount: 1234567,
+      commentCount: 765432,
+      shareCount: 1000,
       comments: [
         {
+          memberId: 12,
           name: "김정희",
-          comment: "뭐",
-          profileimage: `${dog}`,
+          content: "뭐",
+          profileImg: `${dog}`,
           time: new Date() - 10000,
         },
         {
+          memberId: 11,
+
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
         {
+          memberId: 11,
           name: "하성호",
-          comment: "ㅋㅋㅋ",
-          profileimage: `${dog}`,
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
           time: new Date() - 10,
         },
       ],
@@ -137,11 +165,15 @@ function Feed() {
 
   const gotoDetailComment = () => {
     if (currentUrl == "/") {
-      navigate(`/detailcomment/${state.pk}`, { state });
+      navigate(`/detailcomment/${state.id}`, { state });
     }
   };
   const gotoDetailFeed = () => {
-    navigate(`/detailfeed/${state.pk}`, { state });
+    navigate(`/detailfeed/${state.id}`, { state });
+  };
+
+  const gotoEdit = () => {
+    navigate(`/updatereview/${state.id}`, { state });
   };
 
   const formatK = (count) => {
@@ -162,16 +194,20 @@ function Feed() {
       return (c / 60 / 60).toFixed(0) + "시간전 ";
     }
   };
-
+  const formatEmail = (t) => {
+    const s = t.indexOf("@");
+    return t.substring(0, s);
+  };
   const handleAddComment = () => {
     if (newComment.trim() !== "") {
       const updatedComments = [...state.comments];
       updatedComments.push({
-        profileimage: user.profile_img,
-        name: user.id,
-        comment: newComment,
+        profileImg: state,
+        memberId: state.user.id,
+        content: newComment,
+        time: new Date(),
       });
-      state.comment = updatedComments;
+      state.content = updatedComments;
       setState(state);
       setNewComment(""); // 댓글 추가 후 새 댓글 상태 초기화
     }
@@ -191,117 +227,144 @@ function Feed() {
   //         setComments(updatedComments);
   //     }
   // };
-
   return (
-    <div className={styles.container}>
-      {/* 게시글 주인 정보 */}
-      <div className={styles.nameContainer}>
-        <img src={state.profileimage} className={styles.profileimage} />
-        <div className={styles.namebox}>
-          <div className={styles.name}>{state.name}</div>
-          <div className={styles.id}>{state.id}</div>
-        </div>
-      </div>
+    <div>
+      {state.length != 0 ? (
+        <div className={styles.container}>
+          {/* 게시글 주인 정보 */}
+          <div className={styles.nameContainer}>
+            <img src={state.user.profileImg} className={styles.profileimage} />
+            <div className={styles.namebox}>
+              <div className={styles.name}>{state.user.nickname}</div>
+              <div className={styles.id}>{formatEmail(state.user.email)}</div>
+            </div>
+          </div>
 
-      {/* 게시글 이미지 */}
-      <img
-        src={state.image}
-        className={styles.imageContainer}
-        onClick={gotoDetailFeed}
-      />
+          {/* 게시글 이미지 */}
+          <img
+            src={state.thumbnailUrl}
+            className={styles.imageContainer}
+            onClick={gotoDetailFeed}
+          />
 
-      {/* 게시글 좋아요, 댓글, 이어가기 정보 */}
-      <div className={styles.tagContainer}>
-        <div className={styles.tagitem}>
-          <div>
-            {state.isLiked ? (
-              <FavoriteOutlinedIcon style={{ width: "4vw", height: "4vw" }} />
+          {/* 게시글 좋아요, 댓글, 이어가기 정보 */}
+          <div className={styles.tagContainer}>
+            <div className={styles.tagitem}>
+              <div className={styles.tagitemicon}>
+                {state.isLiked ? (
+                  <FavoriteOutlinedIcon
+                    style={{ width: "4vw", height: "4vw" }}
+                  />
+                ) : (
+                  <FavoriteBorderOutlinedIcon
+                    style={{ width: "4vw", height: "4vw" }}
+                  />
+                )}
+              </div>
+              <div>{formatK(state.likeCount)}</div>
+            </div>
+            <div className={styles.tagitem}>
+              <div className={styles.tagitemicon}>
+                <SmsOutlinedIcon style={{ width: "4vw", height: "4vw" }} />
+              </div>
+              <div>{formatK(state.commentCount)}</div>
+            </div>
+            <div className={styles.tagitem}>
+              <div className={styles.tagitemicon}>
+                <ShareOutlinedIcon style={{ width: "4vw", height: "4vw" }} />
+              </div>
+              <div>{formatK(state.shareCount)}</div>
+            </div>
+
+            {/* 수정버튼 */}
+
+            {currentUrl == "/" ? (
+              <></>
             ) : (
-              <FavoriteBorderOutlinedIcon
-                style={{ width: "4vw", height: "4vw" }}
-              />
+              <>
+                {state.length != 0 && user.memberId == state.user.memberId ? (
+                  <EditOutlinedIcon
+                    className={styles.editbtn}
+                    onClick={gotoEdit}
+                  />
+                ) : (
+                  <></>
+                )}
+              </>
             )}
           </div>
-          <div>{formatK(state.likes)}</div>
-        </div>
-        <div className={styles.tagitem}>
-          <div>
-            <SmsOutlinedIcon style={{ width: "4vw", height: "4vw" }} />
+
+          {/* 게시글 본문 */}
+          <div className={styles.contentContainer}>
+            <div className={styles.name}>{state.user.nickname}</div>
+            <div className={styles.content}>{state.content}</div>
           </div>
-          <div>{formatK(state.comment)}</div>
-        </div>
-        <div className={styles.tagitem}>
-          <div>
-            <ShareOutlinedIcon style={{ width: "4vw", height: "4vw" }} />
-          </div>
-          <div>{formatK(state.share)}</div>
-        </div>
-      </div>
 
-      {/* 게시글 본문 */}
-      <div className={styles.contentContainer}>
-        <div className={styles.name}>{state.name}</div>
-        <div className={styles.content}>{state.content}</div>
-      </div>
-
-      {/* 게시글 해시태그 */}
-      <div className={styles.hashTagContainer}>
-        {state.length != 0 &&
-          state.hash.map((v, i) => (
-            <div key={i} className={styles.hash}>
-              <div>{v}</div>
-            </div>
-          ))}
-      </div>
-
-      {/* 게시글 시간 정보 */}
-      <div>{formatT(new Date() - state.time)}</div>
-
-      {/* 게시글 댓글 */}
-      {currentUrl != "/" && state.length != 0 && (
-        <div className={styles.floatBottom}>
-          {state.comments.map((v, i) => (
-            <div key={i} className={styles.commentContainer}>
-              <img src={v.profileimage} className={styles.commentImage} />
-              <div className={styles.commentbox}>
-                <div className={styles.commentInfo}>
-                  <div className={styles.name}>{v.name}</div>
-                  <div className={styles.time}>
-                    {formatT(new Date() - v.time)}
-                  </div>
+          {/* 게시글 해시태그 */}
+          <div className={styles.hashTagContainer}>
+            {state.length != 0 &&
+              state.hashTag.map((v, i) => (
+                <div key={i} className={styles.hash}>
+                  <div>{v}</div>
                 </div>
-                <div className={styles.comment}>{v.comment}</div>
-              </div>
-              {/* {user.id == v.name ? <CloseOutlinedIcon onClick={() => handleDeleteComment(i)} /> : ''} */}
-            </div>
-          ))}
-        </div>
-      )}
+              ))}
+          </div>
 
-      {user && (
-        <div
-          className={
-            currentUrl == "/"
-              ? styles.commentContainer
-              : styles.commentContainerFix
-          }
-        >
-          <img src={user.profile_img} className={styles.commentImage} />
-          <input
-            type="text"
-            className={styles.commentInput}
-            placeholder="댓글 달기..."
-            value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
-            onClick={gotoDetailComment}
-            onKeyDown={(e) => activeEnter(e)}
-          />
-          {newComment.length != 0 && (
-            <button onClick={handleAddComment} className={styles.commentBtn}>
-              추가
-            </button>
+          {/* 게시글 시간 정보 */}
+          <div>{formatT(new Date() - state.time)}</div>
+
+          {/* 게시글 댓글 */}
+          {currentUrl != "/" && state.length != 0 && (
+            <div className={styles.floatBottom}>
+              {state.comments.map((v, i) => (
+                <div key={i} className={styles.commentContainer}>
+                  <img src={v.profileImg} className={styles.commentImage} />
+                  <div className={styles.commentbox}>
+                    <div className={styles.commentInfo}>
+                      <div className={styles.name}>{v.name}</div>
+                      <div className={styles.time}>
+                        {formatT(new Date() - v.time)}
+                      </div>
+                    </div>
+                    <div className={styles.comment}>{v.content}</div>
+                  </div>
+                  {/* {user.id == v.name ? <CloseOutlinedIcon onClick={() => handleDeleteComment(i)} /> : ''} */}
+                </div>
+              ))}
+            </div>
+          )}
+
+          {user && (
+            <div
+              className={
+                currentUrl == "/"
+                  ? styles.commentContainer
+                  : styles.commentContainerFix
+              }
+            >
+              <img src={user.profileImg} className={styles.commentImage} />
+              <input
+                type="text"
+                className={styles.commentInput}
+                placeholder="댓글 달기..."
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+                onClick={gotoDetailComment}
+                onKeyDown={(e) => activeEnter(e)}
+              />
+              {newComment.length != 0 && (
+                <button
+                  onClick={handleAddComment}
+                  className={styles.commentBtn}
+                >
+                  추가
+                </button>
+              )}
+            </div>
           )}
         </div>
+      ) : (
+        <></>
       )}
     </div>
   );
--- frontend/src/pages/DetailFeed/DetailFeed.jsx
+++ frontend/src/pages/DetailFeed/DetailFeed.jsx
@@ -1,94 +1,224 @@
-import styles from './DetailFeed.module.css';
-import { useEffect, useState } from 'react';
-import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import LoopOutlinedIcon from '@mui/icons-material/LoopOutlined';
-import dog from '../../assets/dog.jpg';
+import styles from "./DetailFeed.module.css";
+import { useEffect, useState } from "react";
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import LoopOutlinedIcon from "@mui/icons-material/LoopOutlined";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useNavigate } from "react-router-dom";
+import dog from "../../assets/dog.jpg";
 
 function DetailFeed() {
-    const [user, setUser] = useState(null);
-    const [state, setState] = useState(null);
+  const [user, setUser] = useState(null);
+  const [state, setState] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const user = { id: '@헬린이', profile_img: `${dog}` };
-        setUser(user);
-        const state = {
-            pk: 1,
-            profileimage: `${dog}`,
-            name: '하성호',
-            id: '@헬린이',
-            image: `${dog}`,
-            isLiked: false,
-            isPublic: true,
-            likes: 1234567,
-            comment: 765432,
-            share: 1000,
-            time: new Date() - 400,
-            hash: ['#개', '#댕댕이', '#시바', '#산책'],
-            content: '멍멍',
-            comments: [
-                { name: '김정희', comment: '뭐', profileimage: `${dog}`, time: new Date() - 100 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-                { name: '하성호', comment: 'ㅋㅋㅋ', profileimage: `${dog}`, time: new Date() - 10 },
-            ],
-        };
-        setState(state);
-    }, []);
+  useEffect(() => {
+    const user = { id: "@헬린이", profile_img: `${dog}` };
+    setUser(user);
+    const state = {
+      id: 1,
+      user: {
+        memberId: 12,
+        email: "123@naver.com",
+        nickname: "김정희",
+        profileImg: `${dog}`,
+      },
+      parentId: null,
+      rootId: null,
+      goalContent: "몸짱",
+      content: "안녕하세요!!",
+      thumbnailUrl: `${dog}`,
+      isPrivate: true,
+      isLike: true,
+      time: new Date(),
+      hashTag: ["#운동", "#오운완"],
+      likeCount: 1234567,
+      commentCount: 765432,
+      shareCount: 1000,
+      comments: [
+        {
+          memberId: 12,
+          name: "김정희",
+          content: "뭐",
+          profileImg: `${dog}`,
+          time: new Date() - 10000,
+        },
+        {
+          memberId: 11,
 
-    const download = () => {
-        // 여기서 파일 다운로드 로직 실행
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+        {
+          memberId: 11,
+          name: "하성호",
+          content: "ㅋㅋㅋ",
+          profileImg: `${dog}`,
+          time: new Date() - 10,
+        },
+      ],
     };
+    setState(state);
+  }, []);
+  const setIsPublic = () => {
+    const currentState = { ...state };
+    setState(currentState);
+    if (currentState.isPublic) {
+      currentState.isPublic = false;
+    } else {
+      currentState.isPublic = true;
+    }
+    setState(currentState);
+  };
 
-    const deleteFeed = () => {
-        // 여기서 피드 삭제 로직 실행
-    };
+  const gotodetail = () => {
+    navigate(`/detailcomment/${state.id}`, { state });
+  };
+  const download = () => {
+    // 여기서 파일 다운로드 로직 실행
+  };
 
-    return (
-        <div className={styles.container}>
-            {state && (
-                <>
-                    <img src={state.image} className={styles.imageContainer} />
-                    <div className={styles.etcbox}>
-                        <div className={styles.etcinner}>
-                            <LoopOutlinedIcon className={styles.etcimg} />
-                            댓글보기
-                        </div>
-                        {user.id != state.id && (
-                            <div className={styles.etcinner}>
-                                <LoopOutlinedIcon className={styles.etcimg} />
-                                이어받기
-                            </div>
-                        )}
-                        {user.id == state.id && (
-                            <>
-                                <div className={styles.etcinner} onClick={download}>
-                                    <FileDownloadOutlinedIcon className={styles.etcimg} />
-                                    다운로드
-                                </div>
-                                <div className={styles.etcinner} onClick={deleteFeed}>
-                                    <DeleteOutlinedIcon className={styles.etcimg} />
-                                    삭제
-                                </div>
-                            </>
-                        )}
-                    </div>
-                </>
+  const deleteFeed = () => {
+    // 여기서 피드 삭제 로직 실행
+  };
+
+  return (
+    <div className={styles.container}>
+      {state && (
+        <>
+          <div className={styles.lock}>
+            {user.id != state.user.id ? (
+              <></>
+            ) : state.isPublic ? (
+              <LockOutlinedIcon onClick={setIsPublic} className={styles.icon} />
+            ) : (
+              <LockOpenIcon onClick={setIsPublic} className={styles.icon} />
             )}
-        </div>
-    );
+          </div>
+
+          <img src={state.thumbnailUrl} className={styles.imageContainer} />
+          <div className={styles.etcbox}>
+            <div className={styles.etcinner} onClick={gotodetail}>
+              <LoopOutlinedIcon className={styles.etcimg} />
+              댓글보기
+            </div>
+            {user.id != state.id && (
+              <div className={styles.etcinner}>
+                <LoopOutlinedIcon className={styles.etcimg} />
+                이어받기
+              </div>
+            )}
+            {user.id == state.user.id && (
+              <>
+                <div className={styles.etcinner} onClick={download}>
+                  <FileDownloadOutlinedIcon className={styles.etcimg} />
+                  다운로드
+                </div>
+                <div className={styles.etcinner} onClick={deleteFeed}>
+                  <DeleteOutlinedIcon className={styles.etcimg} />
+                  삭제
+                </div>
+              </>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default DetailFeed;
