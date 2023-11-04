3d864814ecd3024d19ecc58cdb1fec350d75d152
unit74 <u__nit@naver.com>
2023-11-05 03:17:12 +0900

    ✅ Refactor: Add login

--- backend/timing/src/test/java/com/kkukku/timing/apis/comment/services/CommentServiceTest.java
+++ backend/timing/src/test/java/com/kkukku/timing/apis/comment/services/CommentServiceTest.java
@@ -2,25 +2,44 @@ package com.kkukku.timing.apis.comment.services;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
+import com.kkukku.timing.security.services.MemberDetailService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootTest(properties = "spring.profiles.active=local")
 public class CommentServiceTest {
 
     @Autowired
+    private MemberDetailService memberDetailService;
+
+    @Autowired
     private CommentService commentService;
+
+    @BeforeEach
+    public void login() {
+        String email = "unit@com";
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            memberDetailService.loadUserByUsername(email), null,
+            AuthorityUtils.NO_AUTHORITIES);
+
+        SecurityContextHolder.getContext()
+                             .setAuthentication(authentication);
+    }
 
     @Test
     public void saveCommentTest() {
         Long before = commentService.getCommentCountByFeedId(1L);
 
         Long feedId = 1L;
-        Integer memberId = 1;
         String content = "Test";
 
-        commentService.saveComment(feedId, memberId, content);
+        commentService.saveComment(feedId, content);
 
         Long after = commentService.getCommentCountByFeedId(1L);
 
