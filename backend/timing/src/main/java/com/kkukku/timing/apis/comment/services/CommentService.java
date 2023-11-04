9e97ba70e6c79f8cebf786f51f0c959ec35bc010
unit74 <u__nit@naver.com>
2023-11-05 03:18:42 +0900

    🎨 Refactor: Add comment only login member

--- backend/timing/src/main/java/com/kkukku/timing/apis/comment/services/CommentService.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/comment/services/CommentService.java
@@ -3,6 +3,7 @@ package com.kkukku.timing.apis.comment.services;
 import com.kkukku.timing.apis.comment.entities.CommentEntity;
 import com.kkukku.timing.apis.comment.repositories.CommentRepository;
 import com.kkukku.timing.apis.member.services.MemberService;
+import com.kkukku.timing.security.utils.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,9 @@ public class CommentService {
         return commentRepository.countByFeedId(feedId);
     }
 
-    public void saveComment(Long feedId, Integer memberId, String content) {
+    public void saveComment(Long feedId, String content) {
         commentRepository.save(
-            new CommentEntity(feedId, memberService.getMemberById(memberId), content));
+            new CommentEntity(feedId, memberService.getMemberById(
+                SecurityUtil.getLoggedInMemberPrimaryKey()), content));
     }
 }
