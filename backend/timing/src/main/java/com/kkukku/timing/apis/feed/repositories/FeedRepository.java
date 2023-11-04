2c12a6100ef531e2306cc157f8a0e101946807a8
unit74 <u__nit@naver.com>
2023-11-05 01:32:36 +0900

    🐛 Fix: Modify generic types

--- backend/timing/src/main/java/com/kkukku/timing/apis/feed/repositories/FeedRepository.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/feed/repositories/FeedRepository.java
@@ -3,6 +3,6 @@ package com.kkukku.timing.apis.feed.repositories;
 import com.kkukku.timing.apis.feed.entities.FeedEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedRepository extends JpaRepository<Long, FeedEntity> {
+public interface FeedRepository extends JpaRepository<FeedEntity, Long> {
 
 }
