06433da6002cb077b8f96d38c180c4cd8d240471
unit74 <u__nit@naver.com>
2023-11-05 03:32:55 +0900

    ✨ Feat: Add like, dislike features

--- backend/timing/src/main/java/com/kkukku/timing/apis/like/services/LikeService.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/like/services/LikeService.java
@@ -1,6 +1,8 @@
 package com.kkukku.timing.apis.like.services;
 
+import com.kkukku.timing.apis.like.entities.LikeEntity;
 import com.kkukku.timing.apis.like.repositories.LikeRepository;
+import com.kkukku.timing.security.utils.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,30 @@ import org.springframework.stereotype.Service;
 public class LikeService {
 
     private final LikeRepository likeRepository;
-    
+
+    public Long getCommentCountByFeedId(Long feedId) {
+        return likeRepository.countByFeedId(feedId);
+    }
+
+    public Boolean isLiked(Long feedId) {
+        return likeRepository.existsByFeedIdAndMemberId(feedId,
+            SecurityUtil.getLoggedInMemberPrimaryKey());
+    }
+
+    public void saveLike(Long feedId) {
+        if (isLiked(feedId)) {
+            return;
+        }
+
+        likeRepository.save(new LikeEntity(feedId, SecurityUtil.getLoggedInMemberPrimaryKey()));
+    }
+
+    public void deleteLike(Long feedId) {
+        if (!isLiked(feedId)) {
+            return;
+        }
+
+        likeRepository.delete(likeRepository.findByFeedIdAndMemberId(feedId,
+            SecurityUtil.getLoggedInMemberPrimaryKey()));
+    }
 }
