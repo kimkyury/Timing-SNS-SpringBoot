ee25cbcdbf5cf4699a43c5e71e06a6e7184e2e2a
김규리 <mandarining0918@gmail.com>
2023-11-05 02:53:43 +0900

    ✨ feat: Impl CreateMethod with HashTagService

--- backend/timing/src/main/java/com/kkukku/timing/apis/challenge/services/ChallengeService.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/challenge/services/ChallengeService.java
@@ -3,8 +3,10 @@ package com.kkukku.timing.apis.challenge.services;
 import com.kkukku.timing.apis.challenge.entities.ChallengeEntity;
 import com.kkukku.timing.apis.challenge.repositories.ChallengeRepository;
 import com.kkukku.timing.apis.challenge.requests.ChallengeCreateRequest;
+import com.kkukku.timing.apis.hashtag.services.HashTagOptionService;
 import com.kkukku.timing.apis.member.entities.MemberEntity;
 import com.kkukku.timing.apis.member.services.MemberService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,14 @@ public class ChallengeService {
 
     private final MemberService memberService;
     private final ChallengeRepository challengeRepository;
+    private final HashTagOptionService hashTagOptionService;
 
     public void createChallenge(Integer memberId, ChallengeCreateRequest challengeCreateRequest) {
 
         MemberEntity member = memberService.getMemberEntityById(memberId);
+
+        List<String> hashTags = challengeCreateRequest.getHashTags();
+        hashTagOptionService.createHashTagOptions(hashTags);
 
         ChallengeEntity challenge;
         if (challengeCreateRequest.getGoalContent() == null) {
@@ -35,6 +41,5 @@ public class ChallengeService {
 
         challengeRepository.save(challenge);
     }
-
 
 }
