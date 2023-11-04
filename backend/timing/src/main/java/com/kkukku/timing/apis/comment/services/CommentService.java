7993ff123a104f877e65fe6d9e404dce43a8dfb7
unit74 <u__nit@naver.com>
2023-11-05 02:51:47 +0900

    ✨ Feat: Add save comment

--- backend/timing/src/main/java/com/kkukku/timing/apis/comment/services/CommentService.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/comment/services/CommentService.java
@@ -1,6 +1,8 @@
 package com.kkukku.timing.apis.comment.services;
 
+import com.kkukku.timing.apis.comment.entities.CommentEntity;
 import com.kkukku.timing.apis.comment.repositories.CommentRepository;
+import com.kkukku.timing.apis.member.services.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,14 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberService memberService;
+
+    public Long getCommentCountByFeedId(Long feedId) {
+        return commentRepository.countByFeedId(feedId);
+    }
+
+    public void saveComment(Long feedId, Integer memberId, String content) {
+        commentRepository.save(
+            new CommentEntity(feedId, memberService.getMemberById(memberId), content));
+    }
 }
