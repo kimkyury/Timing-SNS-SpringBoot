5951762aa7a66d0ee0f6bce557e379e5958c0434
김규리 <mandarining0918@gmail.com>
2023-11-05 16:32:11 +0900

    🐛 fix: fix S3StartUrl

--- backend/timing/src/main/java/com/kkukku/timing/s3/configs/S3Config.java
+++ backend/timing/src/main/java/com/kkukku/timing/s3/configs/S3Config.java
@@ -19,9 +19,6 @@ public class S3Config {
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${AWS_S3_URL}")
-    private String s3StartUrl;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
