37ef303f1eea47db28f1334c880e74dd1e320f08
unit74 <u__nit@naver.com>
2023-11-05 14:42:36 +0900

    ✅ Test: Add pagination test

--- backend/timing/src/test/java/com/kkukku/timing/apis/comment/services/CommentServiceTest.java
+++ backend/timing/src/test/java/com/kkukku/timing/apis/comment/services/CommentServiceTest.java
@@ -2,7 +2,9 @@ package com.kkukku.timing.apis.comment.services;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
+import com.kkukku.timing.apis.comment.entities.CommentEntity;
 import com.kkukku.timing.security.services.MemberDetailService;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,15 +36,31 @@ public class CommentServiceTest {
 
     @Test
     public void saveCommentTest() {
-        Long before = commentService.getCommentCountByFeedId(1L);
-
         Long feedId = 1L;
         String content = "Test";
+        Long before = commentService.getCommentCountByFeedId(feedId);
 
         commentService.saveComment(feedId, content);
 
-        Long after = commentService.getCommentCountByFeedId(1L);
+        Long after = commentService.getCommentCountByFeedId(feedId);
 
         assertEquals("Compare with before count and after count", before + 1, after);
+    }
+
+    @Test
+    public void getCommentsByFeedIdTest() {
+        Long feedId = 1L;
+        Integer pageSize = 10;
+
+        for (int i = 0; i < 100; i++) {
+            commentService.saveComment(feedId, "Test");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            List<CommentEntity> comments = commentService.getCommentsByFeedId(feedId, i + 1,
+                pageSize);
+
+            assertEquals("Check only 10 rows", pageSize, comments.size());
+        }
     }
 }
