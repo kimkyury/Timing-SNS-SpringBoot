f4c19745993e7ad07fce46103503c1273adba893
김규리 <mandarining0918@gmail.com>
2023-11-05 19:56:00 +0900

    ✨Feat: Implement GetChallenge API

--- backend/timing/src/main/java/com/kkukku/timing/apis/challenge/services/ChallengeService.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/challenge/services/ChallengeService.java
@@ -3,13 +3,16 @@ package com.kkukku.timing.apis.challenge.services;
 import com.kkukku.timing.apis.challenge.entities.ChallengeEntity;
 import com.kkukku.timing.apis.challenge.repositories.ChallengeRepository;
 import com.kkukku.timing.apis.challenge.requests.ChallengeCreateRequest;
+import com.kkukku.timing.apis.challenge.responses.ChallengeResponse;
+import com.kkukku.timing.apis.challenge.responses.ChallengeResponse.Challenge;
 import com.kkukku.timing.apis.hashtag.entities.HashTagOptionEntity;
 import com.kkukku.timing.apis.hashtag.services.ChallengeHashTagService;
 import com.kkukku.timing.apis.hashtag.services.HashTagOptionService;
 import com.kkukku.timing.apis.member.entities.MemberEntity;
 import com.kkukku.timing.apis.member.services.MemberService;
-import com.kkukku.timing.exception.CustomException;
-import com.kkukku.timing.response.codes.ErrorCode;
+import com.kkukku.timing.s3.services.S3Service;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,7 @@ public class ChallengeService {
     private final ChallengeRepository challengeRepository;
     private final HashTagOptionService hashTagOptionService;
     private final ChallengeHashTagService challengeHashTagService;
+    private final S3Service s3Service;
 
     public void createChallengeProcedure(Integer memberId,
         ChallengeCreateRequest challengeCreateRequest) {
@@ -46,11 +50,34 @@ public class ChallengeService {
     }
 
 
-    public ChallengeEntity getChallengeEntity(Long challengeId) {
+    public ChallengeResponse getChallenge(Integer memberId) {
 
-        return challengeRepository.findById(challengeId)
-                                  .orElseThrow(() -> new CustomException(
-                                      ErrorCode.NOT_EXIST_CHALLENGE));
+        return new ChallengeResponse(
+            challengeRepository.findByMemberId(memberId)
+                               .stream()
+                               .map(c -> {
+                                   String thumbnailUrl =
+                                       s3Service.getS3StartUrl() + c.getThumbnailUrl();
+
+                                   System.out.println(c.getStartedAt() + " " + LocalDate.now()
+                                                                                        .minusDays(
+                                                                                            1));
+                                   int countDays = diffDay(c.getStartedAt(), LocalDate.now()
+                                                                                      .minusDays(
+                                                                                          1));
+                                   int maxDays = diffDay(c.getStartedAt(), c.getEndedAt());
+
+                                   System.out.println("result: " + countDays + " " + maxDays);
+                                   return new Challenge(thumbnailUrl, countDays,
+                                       maxDays);
+                               })
+                               .toList());
+    }
+
+
+    public int diffDay(LocalDate startedAt, LocalDate yesterday) {
+        return (int) ChronoUnit.DAYS.between(startedAt, yesterday);
+
     }
 
 }
