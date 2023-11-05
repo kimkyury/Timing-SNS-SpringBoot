c17a12d9ba844edf3736eee6fef81d56d46bb6aa
김규리 <mandarining0918@gmail.com>
2023-11-05 16:59:38 +0900

    🐛 fix: Fix Member Entity (isDelete, and DeleteResponse)

--- backend/timing/src/main/java/com/kkukku/timing/apis/member/responses/MemberDetailResponse.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/member/responses/MemberDetailResponse.java
@@ -1,18 +1,24 @@
 package com.kkukku.timing.apis.member.responses;
 
 
+import com.kkukku.timing.apis.member.entities.MemberEntity;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberDetailResponse {
 
     private String email;
     private String nickname;
     private String profileImageUrl;
-    private boolean isDelete;
+    private Boolean isDelete;
+
+    public MemberDetailResponse(MemberEntity member) {
+        this.email = member.getEmail();
+        this.nickname = member.getNickname();
+        this.profileImageUrl = member.getProfileImageUrl();
+        this.isDelete = member.getIsDelete();
+    }
 }
