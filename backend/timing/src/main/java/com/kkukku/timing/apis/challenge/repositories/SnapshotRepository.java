ffab3746399f913f5355351ed2831995afa5430b
김규리 <mandarining0918@gmail.com>
2023-11-06 17:00:54 +0900

    ✨Feat: Implement Snapshot GetAllByChallenge Method

---
+++ backend/timing/src/main/java/com/kkukku/timing/apis/challenge/repositories/SnapshotRepository.java
@@ -0,0 +1,10 @@
+package com.kkukku.timing.apis.challenge.repositories;
+
+import com.kkukku.timing.apis.challenge.entities.SnapshotEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SnapshotRepository extends JpaRepository<SnapshotEntity, Long> {
+
+    List<SnapshotEntity> findAllByChallengeId(Long challengeId);
+}
