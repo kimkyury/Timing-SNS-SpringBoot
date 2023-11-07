00661d6a6c59f3f2c8f96444bd3c529ee2a7b4a6
unit74 <u__nit@naver.com>
2023-11-07 17:16:57 +0900

    🎨 Refactor: Combine similar DTOs

--- backend/timing/src/main/java/com/kkukku/timing/apis/feed/responses/FeedSummaryWithCountResponse.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/feed/responses/FeedSummaryWithCountResponse.java
@@ -1,23 +1,24 @@
 package com.kkukku.timing.apis.feed.responses;
 
-import com.kkukku.timing.apis.feed.entities.FeedEntity;
+
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FeedOtherResponse {
+public class FeedSummaryWithCountResponse {
 
-    private Long id;
-    private String thumbnailUrl;
+    private List<FeedSummaryResponse> feeds;
     private Long feedCount;
     private Integer contributeCount;
 
-    public FeedOtherResponse(FeedEntity feed, Long feedCount, Integer contributeCount) {
-        this.id = feed.getId();
-        this.thumbnailUrl = feed.getThumbnailUrl();
+    public FeedSummaryWithCountResponse(List<FeedSummaryResponse> feeds, Long feedCount,
+        Integer contributeCount) {
+        this.feeds = feeds;
         this.feedCount = feedCount;
         this.contributeCount = contributeCount;
     }
+
 }
