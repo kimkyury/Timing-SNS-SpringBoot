93de3c6c43367ba697af965d30b7320256cd96b0
Kim JeongHui <spor1998@naver.com>
2023-11-07 10:45:27 +0900

    :poop: Fix: fix marge

--- frontend/src/components/TimeLapse/TimeLapse.jsx
+++ frontend/src/components/TimeLapse/TimeLapse.jsx
@@ -5,12 +5,12 @@ import 'react-circular-progressbar/dist/styles.css';
 import dog from '../../assets/dog.jpg';
 import dog2 from '../../assets/dog2.jpg';
 import { useEffect, useState } from 'react';
-import ConfettiExplosion from "react-confetti-explosion";
+import ConfettiExplosion from 'react-confetti-explosion';
 
 function TimeLapse() {
-  const navigate = useNavigate();
-  const [timeLaps, setTimeLaps] = useState([]);
-  const [isFinished, setIsFinished] = useState(false);
+    const navigate = useNavigate();
+    const [timeLaps, setTimeLaps] = useState([]);
+    const [isFinished, setIsFinished] = useState(false);
 
     useEffect(() => {
         // 여기서 axios로 timeLaps 가져올꺼임
@@ -23,12 +23,12 @@ function TimeLapse() {
             { id: 1, percent: 70, img: `${dog}` },
         ];
 
-    setTimeLaps(state);
-  }, []);
+        setTimeLaps(state);
+    }, []);
 
-  const finishTimeLaps = () => {
-    setIsFinished(!isFinished);
-  };
+    const finishTimeLaps = () => {
+        setIsFinished(!isFinished);
+    };
 
     const takePhoto = (index) => {
         navigate(`/jeonghui`, { state: timeLaps[index] });
@@ -39,80 +39,72 @@ function TimeLapse() {
         // }
     };
 
+    const continueTimeLaps = () => {
+        // 타입랩스 이어가기 위한 로직 추가
+        setIsFinished(false);
+    };
 
-  const continueTimeLaps = () => {
-    // 타입랩스 이어가기 위한 로직 추가
-    setIsFinished(false);
-  };
-
-
-  const discontinueTimeLaps = () => {
-    // 타입랩스를 피드로 변환하는 로직 추가
-    setIsFinished(false);
-  };
-  const largeProps = {
-    force: 0.8,
-    duration: 3000,
-    particleCount: 300,
-    width: 1600,
-    colors: ["#041E43", "#1471BF", "#5BB4DC", "#FC027B", "#66D805"],
-  };
-  return (
-    <div className={styles.container}>
-      {timeLaps.map((element, index) => (
-        <div key={index} className={styles.timeLaps}>
-          <div className={styles.circularContainer} onClick={finishTimeLaps}>
-            <CircularProgressbarWithChildren
-              value={element.percent}
-              strokeWidth="10"
-              styles={buildStyles({ pathColor: "red", textColor: "black" })}
-            >
-              <img src={element.img} className={styles.timeLapsImage} />
-            </CircularProgressbarWithChildren>
-          </div>
-        </div>
-      ))}
-      <div className={styles.timeLaps} onClick={() => navigate(`/create`)}>
-        <div className={styles.timeLaps}>
-          <CircularProgressbar
-            value="40"
-            text="+"
-            strokeWidth="10"
-            styles={buildStyles({
-              pathColor: "red",
-              textColor: "black",
-              textSize: "50px",
-            })}
-          />
-        </div>
-      </div>
-
-      {isFinished && (
-        <div className={styles.finish}>
-          <ConfettiExplosion {...largeProps} />
-          <div className={styles.finishContainer}>
-            <div>타임랩스를 완료했습니다!!</div>
-            <div>이어하시면 21일이 추가됩니다.</div>
-            <div>이어하시겠습니까?</div>
-            <div className={styles.buttonContainer}>
-              <button
-                className={styles.continueTimeLaps}
-                onClick={continueTimeLaps}
-              >
-                예
-              </button>
-              <button
-                className={styles.discontinueTimeLaps}
-                onClick={discontinueTimeLaps}
-              >
-                아니요
-              </button>
+    const discontinueTimeLaps = () => {
+        // 타입랩스를 피드로 변환하는 로직 추가
+        setIsFinished(false);
+    };
+    const largeProps = {
+        force: 0.8,
+        duration: 3000,
+        particleCount: 300,
+        width: 1600,
+        colors: ['#041E43', '#1471BF', '#5BB4DC', '#FC027B', '#66D805'],
+    };
+    return (
+        <div className={styles.container}>
+            {timeLaps.map((element, index) => (
+                <div key={index} className={styles.timeLaps}>
+                    <div className={styles.circularContainer} onClick={() => takePhoto(index)}>
+                        <CircularProgressbarWithChildren
+                            value={element.percent}
+                            strokeWidth="10"
+                            styles={buildStyles({ pathColor: 'red', textColor: 'black' })}
+                        >
+                            <img src={element.img} className={styles.timeLapsImage} />
+                        </CircularProgressbarWithChildren>
+                    </div>
+                </div>
+            ))}
+            <div className={styles.timeLaps} onClick={() => navigate(`/create`)}>
+                <div className={styles.timeLaps}>
+                    <CircularProgressbar
+                        value="40"
+                        text="+"
+                        strokeWidth="10"
+                        styles={buildStyles({
+                            pathColor: 'red',
+                            textColor: 'black',
+                            textSize: '50px',
+                        })}
+                    />
+                </div>
             </div>
-          </div>
+
+            {isFinished && (
+                <div className={styles.finish}>
+                    <ConfettiExplosion {...largeProps} />
+                    <div className={styles.finishContainer}>
+                        <div>타임랩스를 완료했습니다!!</div>
+                        <div>이어하시면 21일이 추가됩니다.</div>
+                        <div>이어하시겠습니까?</div>
+                        <div className={styles.buttonContainer}>
+                            <button className={styles.continueTimeLaps} onClick={continueTimeLaps}>
+                                예
+                            </button>
+                            <button className={styles.discontinueTimeLaps} onClick={discontinueTimeLaps}>
+                                아니요
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default TimeLapse;
