ef7e33a4af132ee12fe3854c405a386f879e0546
unknown <tgx963@naver.com>
2023-11-07 17:53:49 +0900

    ✨ Feat: connect flike api

--- frontend/src/pages/DetailComment/DetailComment.jsx
+++ frontend/src/pages/DetailComment/DetailComment.jsx
@@ -1,21 +1,36 @@
-import styles from './DetailComment.module.css';
-import Feed from '../../components/Feed/Feed';
-import { useLocation } from 'react-router';
-import { useEffect } from 'react';
-
+import styles from "./DetailComment.module.css";
+import Feed from "../../components/Feed/Feed";
+import { useLocation } from "react-router";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function DetailComment() {
-    const location = useLocation();
-    const state = location.state;
+  const location = useLocation();
+  const state = location.state;
+  const [user, setUser] = useState(null);
+  const BASE_URL = `http://k9e203.p.ssafy.io`;
+  const [accessToken, setAccessToken] = useState(
+    sessionStorage.getItem("accessToken")
+  );
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/api/v1/members`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        console.log(state);
-    }, []);
-
-    return (
-        <div className={styles.container}>
-            <Feed state={state} />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Feed data={state} />
+    </div>
+  );
 }
 
 export default DetailComment;
