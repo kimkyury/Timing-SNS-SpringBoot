2e9c3289ebf3861857b3263ea26a5b8dcb6dc159
unit74 <u__nit@naver.com>
2023-11-10 17:55:04 +0900

    ✅ Test: Add feed dummy

--- backend/timing/src/main/java/com/kkukku/timing/apis/test/controllers/TestController.java
+++ backend/timing/src/main/java/com/kkukku/timing/apis/test/controllers/TestController.java
@@ -3,11 +3,22 @@ package com.kkukku.timing.apis.test.controllers;
 import static com.kkukku.timing.response.ApiResponseUtil.success;
 
 import com.amazonaws.services.s3.model.S3Object;
+import com.kkukku.timing.apis.comment.services.CommentService;
+import com.kkukku.timing.apis.feed.repositories.FeedRepository;
+import com.kkukku.timing.apis.feed.services.FeedService;
+import com.kkukku.timing.apis.hashtag.services.FeedHashTagService;
+import com.kkukku.timing.apis.like.services.LikeService;
+import com.kkukku.timing.apis.member.repositories.MemberRepository;
+import com.kkukku.timing.apis.test.requests.FeedDummyRequest;
+import com.kkukku.timing.apis.test.responses.FeedResponse;
+import com.kkukku.timing.apis.test.responses.MemberResponse;
 import com.kkukku.timing.response.ApiResponseUtil;
 import com.kkukku.timing.response.codes.ErrorCode;
 import com.kkukku.timing.s3.services.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
@@ -29,6 +40,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class TestController {
 
     private final S3Service s3Service;
+    private final FeedService feedService;
+    private final FeedRepository feedRepository;
+    private final MemberRepository memberRepository;
+    private final FeedHashTagService feedHashTagService;
+    private final LikeService likeService;
+    private final CommentService commentService;
 
     @Operation(summary = "응답테스트", tags = {"0. Test"})
     @GetMapping("/ping")
@@ -62,6 +79,44 @@ public class TestController {
                              .header(HttpHeaders.CONTENT_DISPOSITION,
                                  "attachment; filename=\"" + fileName + "\"")
                              .body(resource);
+
+    }
+
+    @GetMapping(value = "/feeds")
+    public ResponseEntity<List<FeedResponse>> getFeeds() {
+        return ApiResponseUtil.success(feedRepository.findAll()
+                                                     .stream()
+                                                     .map(
+                                                         feed -> new FeedResponse(feed,
+                                                             feedHashTagService.getHashTagsByFeedId(
+                                                                 feed.getId()), s3Service))
+                                                     .toList());
+    }
+
+    @GetMapping(value = "/members")
+    public ResponseEntity<List<MemberResponse>> getMembers() {
+        return ApiResponseUtil.success(memberRepository.findAll()
+                                                       .stream()
+                                                       .map(MemberResponse::new)
+                                                       .toList());
+    }
+
+    @PostMapping(value = "/feeds", consumes = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.MULTIPART_FORM_DATA_VALUE
+    })
+    public ResponseEntity<?> saveFeed(
+        @RequestPart List<MultipartFile> snapshots,
+        @RequestPart @Valid FeedDummyRequest feedDummyRequest) {
+        if (snapshots.size() % 21 != 0) {
+            return ResponseEntity.badRequest()
+                                 .body("스냅샷 개수가 21의 배수만 가능");
+        }
+
+        System.out.println(feedDummyRequest);
+        System.out.println(snapshots);
+
+        return ApiResponseUtil.success();
 
     }
 }
