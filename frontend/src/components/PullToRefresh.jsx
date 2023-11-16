8f7893462d8336098592d004357f629409c5f13b
unknown <tgx963@naver.com>
2023-11-16 16:23:07 +0900

    :poop:Fix:fixptr

--- frontend/src/components/PullToRefresh.jsx
+++ frontend/src/components/PullToRefresh.jsx
@@ -4,6 +4,7 @@ import { CircularProgress } from '@mui/material';
 const PullToRefresh = ({ onRefresh, children }) => {
     const [refreshing, setRefreshing] = useState(false);
     const [startY, setStartY] = useState(0);
+
     const handleTouchStart = (event) => {
         setStartY(event.touches[0].clientY);
     };
@@ -21,35 +22,35 @@ const PullToRefresh = ({ onRefresh, children }) => {
                 setRefreshing(true);
             }
         }
-
-        const handleTouchEnd = () => {
-            if (refreshing) {
-                onRefresh();
-                setRefreshing(false);
-            }
-            setStartY(0);
-        };
-
-        return (
-            <div
-                style={{
-                    overflow: 'auto',
-                    WebkitOverflowScrolling: 'touch',
-                }}
-                onTouchStart={handleTouchStart}
-                onTouchMove={handleTouchMove}
-                onTouchEnd={handleTouchEnd}
-            >
-                {refreshing && (
-                    <div style={{ height: '20vw', textAlign: 'center', color: 'black' }}>
-                        <CircularProgress color="inherit" style={{ width: '20vw', height: '20vw' }} />
-                    </div>
-                )}
-
-                {children}
-            </div>
-        );
     };
+
+    const handleTouchEnd = () => {
+        if (refreshing) {
+            onRefresh();
+            setRefreshing(false);
+        }
+        setStartY(0);
+    };
+
+    return (
+        <div
+            style={{
+                overflow: 'auto',
+                WebkitOverflowScrolling: 'touch',
+            }}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+        >
+            {refreshing && (
+                <div style={{ height: '20vw', textAlign: 'center', color: 'black' }}>
+                    <CircularProgress color="inherit" style={{ width: '20vw', height: '20vw' }} />
+                </div>
+            )}
+
+            {children}
+        </div>
+    );
 };
 
 export default PullToRefresh;
